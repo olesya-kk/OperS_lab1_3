32f45af7d40d818e73c2208b5df7bc656c6cc3c5
Admin <Admin@LAPTOP-Q6I9MM59>
2024-02-11 19:25:43 +0300

    Добавьте файлы проекта.

---
+++ OperS_lab1_3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OperS_lab1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            textBox1.Text = "Пришел";
+            textBox2.Text = "Ушел";
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void textBox2_MouseHover(object sender, EventArgs e)
+        {
+            textBox1.Text = "Ушел";
+            textBox2.Text = "Пришел";
+
+        }
+
+        private void textBox2_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
